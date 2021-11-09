a1f124881727462670855c53fa67f6881abc862b
HungLV <hunglv@piv.asia>
2021-11-09 16:55:58 +0700

    dataset return mask in train/test/val stage only

--- datasets/CauGiayDataset.py
+++ datasets/CauGiayDataset.py
@@ -5,19 +5,21 @@ from glob import glob
 import torch
 from torch.utils.data import Dataset
 
+
 class CauGiayDataset(Dataset):
     def __init__(
         self, 
         root_dir, 
         boundary_kernel_size = None,
         transforms=None,
+        stage="train"
         ):
 
+        self.stage = stage
         self.root_dir = root_dir
         self.inputs = glob(os.path.join(self.root_dir, "images", "*.npy"))
 
         self.transforms = transforms
-
         self.generate_boundary = boundary_kernel_size is not None
 
     def __len__(self,):
@@ -31,30 +33,34 @@ class CauGiayDataset(Dataset):
         image = np.load(image_filepath)
         image = torch.Tensor(image).permute(2, 0, 1)[None, :, :, :] ## Converts to 1,C,H,W
 
-        # Load masks
-        mask = np.load(os.path.join(self.root_dir, "masks", image_filename.replace(".npy", "_mask.npy")))
-        mask = (mask > 0).astype(np.int32)
-        mask = torch.Tensor(mask)[None, None, :, :] ## 1, 1, H, W
-
         # Apply transforms if any
         if self.transforms is not None:
             for transform in self.transforms:
                 image = transform(image)
-                mask = transform(mask)
 
         # Convert image to [0 1] and C, H, W
         image = image.squeeze()
         image = image.float() / 255.0 # Converts image from [0 255] to [0 1] fp
+        batch = {'image': image}
 
         # Mask of shape H, W
-        mask = torch.ceil(mask).squeeze()
+        if self.stage in ["train", "test", "val"]:
+            # Load masks
+            mask = np.load(os.path.join(self.root_dir, "masks", image_filename.replace(".npy", "_mask.npy")))
+            mask = (mask > 0).astype(np.int32)
+            mask = torch.Tensor(mask)[None, None, :, :] ## 1, 1, H, W
 
-        batch = {'image': image, 'mask': mask}
+            if self.transforms is not None:
+                for transform in self.transforms:
+                    mask = transform(mask)
+        
+            mask = torch.ceil(mask).squeeze()
+            batch["mask"] = mask
 
-        # Generate boundary if specified
-        if self.generate_boundary:
-            mask_wt = self.process_boundary(image_filename)
-            batch['boundary'] = mask_wt
+            # Generate boundary if specified
+            if self.generate_boundary:
+                mask_wt = self.process_boundary(image_filename)
+                batch['boundary'] = mask_wt
         
         return batch
 
