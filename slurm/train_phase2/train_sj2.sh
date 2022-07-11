58eec175977e0613feba2aa888b776ab017dfa81
Nathan Jo <nathanjo@sls-314-mbpro2021.myfiosgateway.com>
2022-07-11 11:08:12 -0400

    new train

--- slurm/train_phase2/train_sj2.sh
+++ slurm/train_phase2/train_sj2.sh
@@ -7,10 +7,11 @@
 #SBATCH --time=12:00:00
 #SBATCH --mem=50GB
 #SBATCH --gres=gpu:2
+#SBATCH --dependency=afterany:57538122
 
 cd ../../
 
 singularity exec --nv $GROUP_HOME/singularity/rgb-building1.sif python3 run_deeplab.py --backbone=drn_c42 --out-stride=8 --dataset=OSM \
-    --workers=4 --loss-type=wce_dice --fbeta=0.3 --epochs=120 --batch-size=2 --test-batch-size=4 --weight-decay=1e-4 \
+    --workers=4 --loss-type=wce_dice --fbeta=0.1 --epochs=120 --batch-size=2 --test-batch-size=4 --weight-decay=1e-4 \
     --gpu-ids=0,1 --lr=1e-4 --loss-weights 1.0 1.0 --dropout 0.3 0.5 --incl-bounds --resume=crowdAI --best-miou --freeze-bn \
     --data-root=/oak/stanford/groups/deho/building_compliance/san_jose_naip_512/phase2/ --loss-weights-param=1.05 --preempt-robust
