f07d14b46ed07ef0cc3c5c54992ea5a9c5019857
Andrea Vallebueno <avaimar@sh02-ln04.stanford.edu>
2022-07-21 15:36:34 -0700

    W&B with preempting, summary stats

--- models/utils/wandb_utils.py
+++ models/utils/wandb_utils.py
@@ -2,9 +2,6 @@ import wandb
 
 
 def wandb_segmentation_image(input_img, pred_mask, gt_mask, class_labels):
-    print(input_img.shape)
-    print(pred_mask.shape)
-    print(gt_mask.shape)
     return wandb.Image(input_img, masks={
         'predictions': {
             "mask_data": pred_mask,
