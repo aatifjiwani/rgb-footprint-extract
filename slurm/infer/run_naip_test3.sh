11c37f493a71eb23ff94c4fcb3098dc2de2aa8e5
Nathan Jo <nathanjo@sls-314-mbpro2021.local>
2022-07-24 15:56:54 -0400

    superres infer epoch 11

--- slurm/infer/run_naip_test3.sh
+++ slurm/infer/run_naip_test3.sh
@@ -11,8 +11,8 @@
 cd ../../
 
 singularity exec --nv $GROUP_HOME/singularity/rgb-building1.sif python3 run_deeplab.py --inference --backbone=drn_c42 --out-stride=8 \
-    --workers=2 --epochs=1 --test-batch-size=1 --gpu-ids=0 --resume=SJ_OSM_0.1_True_0.0001_0.0001_1.05_crowdAI --best-miou \
+    --workers=2 --epochs=1 --test-batch-size=1 --gpu-ids=0 --resume=SJ_0.2_True_0.0001_0.0001_1.03_superres --best-miou \
     --window-size=256 --stride=256 \
     --input-filename='/oak/stanford/groups/deho/building_compliance/san_jose_naip_512/train/images/m_3712141_se_10_060_20200525_134.npy' \
-    --output-filename='p2_sj_adu_0.1_1.05_miou.npy'
+    --output-filename='p2_sj_adu_0.2_1.03_superres.npy'
 
