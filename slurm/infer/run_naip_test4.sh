17115e430c9b8d3acf406b5d3909c2cff78015fd
Nathan Jo <nathanjo@sls-314-mbpro2021.local>
2022-08-02 17:19:14 -0700

    new slurm

--- slurm/infer/run_naip_test4.sh
+++ slurm/infer/run_naip_test4.sh
@@ -13,5 +13,5 @@ cd ../../
 singularity exec --nv $GROUP_HOME/singularity/rgb-building1.sif python3 run_deeplab.py --inference --backbone=drn_c42 --out-stride=8 \
     --workers=2 --epochs=1 --test-batch-size=1 --gpu-ids=0 --resume=SJ_0.1_True_0.0005_0.0001_1.0_20_superresx4 \
     --window-size=256 --stride=256 --best-miou \
-    --input-filename='/oak/stanford/groups/deho/building_compliance/san_jose_naip_512/phase2_superresx4/temp/1.npy' \
-    --output-filename='p2_sj_adu_superresx4_1.npy'
+    --input-filename='/oak/stanford/groups/deho/building_compliance/san_jose_naip_512/phase2_superresx4/temp/3.npy' \
+    --output-filename='p2_sj_adu_superresx4_3.npy'
