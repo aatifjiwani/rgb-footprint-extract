a12895886f338b89ae2e4111b1bb6583be9881e3
Andrea Vallebueno <avaimar@sls-315-M1.local>
2022-08-04 20:36:33 -0700

    SMIOU implementation

--- models/deeplab/train.py
+++ models/deeplab/train.py
@@ -149,12 +149,12 @@ class Trainer(object):
                             val_metrics_historical['pixel_acc'].append(l['val_pixel_acc'])
                             val_metrics_historical['f1'].append(l['val_f1'])
                             if 'mIOU-SB' in l:
-                                val_metrics_historical['mIOU-SB'].append(l['mIOU-SB'])
+                                val_metrics_historical['mIOU-SB'].append(l['val_mIOU-SB'])
                                 for buffer in SMALL_BUILDING_BUFFERS:
                                     val_metrics_historical['SmIoU-V1-{}'.format(buffer)].append(
-                                        l['SmIoU-V1-{}'.format(buffer)])
+                                        l['val_SmIoU-V1-{}'.format(buffer)])
                                     val_metrics_historical['SmIoU-V2-{}'.format(buffer)].append(
-                                        l['SmIoU-V2-{}'.format(buffer)])
+                                        l['val_SmIoU-V2-{}'.format(buffer)])
 
                         elif 'train_loss' in l:
                             if l['epoch'] not in train_metrics_historical:
@@ -193,11 +193,11 @@ class Trainer(object):
                                         'val_pixel_acc': val_metrics_historical['pixel_acc'][e], 
                                         'val_f1': val_metrics_historical['f1'][e], 'epoch': e}
                             if 'mIOU-SB' in val_metrics_historical.keys():
-                                dic_line['mIOU-SB'] = val_metrics_historical['mIOU-SB'][e]
+                                dic_line['val_mIOU-SB'] = val_metrics_historical['mIOU-SB'][e]
                                 for buffer in SMALL_BUILDING_BUFFERS:
-                                    dic_line['SmIoU-V1-{}'.format(buffer)] = \
+                                    dic_line['val_SmIoU-V1-{}'.format(buffer)] = \
                                         val_metrics_historical['SmIoU-V1-{}'.format(buffer)][e]
-                                    dic_line['SmIoU-V2-{}'.format(buffer)] = \
+                                    dic_line['val_SmIoU-V2-{}'.format(buffer)] = \
                                         val_metrics_historical['SmIoU-V2-{}'.format(buffer)][e]
                             json.dump(dic_line, f)
                             f.write('\n')
@@ -229,11 +229,11 @@ class Trainer(object):
                             "val_pixel_acc": val_metrics_historical['pixel_acc'][epoch],
                             "val_f1": val_metrics_historical['f1'][epoch]}
                         if 'mIOU-SB' in val_metrics_historical.keys():
-                            val_metrics['mIOU-SB'] = val_metrics_historical['mIOU-SB'][epoch]
+                            val_metrics['val_mIOU-SB'] = val_metrics_historical['mIOU-SB'][epoch]
                             for buffer in SMALL_BUILDING_BUFFERS:
-                                val_metrics['SmIoU-V1-{}'.format(buffer)] = \
+                                val_metrics['val_SmIoU-V1-{}'.format(buffer)] = \
                                     val_metrics_historical['SmIoU-V1-{}'.format(buffer)][epoch]
-                                val_metrics['SmIoU-V2-{}'.format(buffer)] = \
+                                val_metrics['val_SmIoU-V2-{}'.format(buffer)] = \
                                     val_metrics_historical['SmIoU-V2-{}'.format(buffer)][epoch]
 
                         # Save val metrics
